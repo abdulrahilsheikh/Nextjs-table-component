a6b8874a23ea4d5949e0896d4476b72b4bea0d90
abdulrahilsheikh <rahilsheikh67064@gmail.com>
2023-07-16 00:07:26 +0530

    test: jsx componnets in rows test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,40 @@
 import DataTable from "@/componnets/DataTable/DataTable";
-import { headers, rows } from "@/mockdata/DataTable.mockdata";
+import { IDataTableRow } from "@/componnets/DataTable/DataTable.types";
+import { headers, rows as data } from "@/mockdata/DataTable.mockdata";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { Badge, Box, ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+const StatusBadge = ({ status }: any) => {
+  const data = {
+    pending: "teal",
+  };
+  return (
+    <Badge borderRadius="full" px="2" colorScheme="teal">
+      New
+    </Badge>
+  );
+};
 
 export default function Home() {
+  const [rows, setRows] = useState<IDataTableRow[]>([]);
+
+  useEffect(() => {
+    const temp = data.map((item) => {
+      item.Status = <StatusBadge />;
+      return item;
+    });
+
+    setRows(temp);
+  }, []);
+
   return (
     <ChakraProvider>
-      <DataTable headers={headers} rows={rows} />
+      <Box padding={8}>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <DataTable sorting pagination headers={headers} rows={rows} />
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
